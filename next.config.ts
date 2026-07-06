7e377106bd8e7a7c2061693b47c39d8e5d337b5e
SaraG <goossens.saraa@gmail.com>
2026-07-07 01:07:18 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "/Users/saragoossens/Downloads/studiosago",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;

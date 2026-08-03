19b0b01190730f6cbaec4dd1c4b304387659391e
SaraG <goossens.saraa@gmail.com>
2026-08-03 19:25:22 +0200

    reset

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+import "./.next/dev/types/routes.d.ts";
 
-// Dit bestand wordt automatisch gebruikt door Next.js.
-// Pas dit bestand niet handmatig aan.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
